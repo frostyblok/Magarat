04c10756e72d57fce9e141febe45487483a2d5e8
Oluwakunle Fakorede <oluwakunlefakorede@Oluwakunles-MacBook-Pro.local>
2019-06-06 02:53:32 +0100

    fix(CI/CD): fix gem install issues with circleci - update docker image and upgrade bundler - add circleci badge to project

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe SearchController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do
-      get :index
+      get :index, params: {query: '#meee'}
       expect(response).to have_http_status(:success)
     end
   end
